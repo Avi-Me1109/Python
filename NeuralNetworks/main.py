29731012dc212e18960530730dfc942055048a64
Avi Raj <aviraj911@hotmail.com>
2023-01-31 13:02:50 +0800

    Network changes

--- NeuralNetworks/main.py
+++ NeuralNetworks/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import stats
+import matplotlib.pyplot as plt
 
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
@@ -13,3 +14,8 @@ x = np.std(speed)
 print(x)
 x = np.var(speed)
 print(x)
+x = np.percentile(speed, 75)
+print(x)
+x = np.random.uniform(0.0, 5.0, 250)
+plt.hist(x, 5)
+plt.show
